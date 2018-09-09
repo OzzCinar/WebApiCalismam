384daf1597378d76d666ff99aec4b0f0e22a3697
Özgür <ozzcinar7@gmail.com>
2018-09-09 18:36:33 +0300

    add,

---
+++ WebApiFirstWork/Controllers/FirsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApiFirstWork.Controllers
+{
+    
+    public class FirsController : ApiController
+    {
+        [HttpGet]
+        public string[] Sehirler ()
+        {
+            return new string[] {"Ankara", "Samsun" ,  "Ordu"};  
+        }
+
+
+    }
+}
---
+++ WebApiFirstWork/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApiFirstWork.WebApiApplication" Language="C#" %>
